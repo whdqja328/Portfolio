6c201bd00a5ff0c45ca30548b4032b2aa6cf287b
whdqja328 <whdqja328@naver.com>
2021-06-17 10:43:25 +0900

    JS_remove_active

--- assets/js/main.js
+++ assets/js/main.js
@@ -63,6 +63,13 @@ workBtnContainer.addEventListener('click', (e) => {
     if (filter == null) {
         return;
     }
+
+    // 해당 페이지 클래스 추가
+    const active = document.querySelector('.category_btn.active');
+    active.classList.remove('active')
+    const target = e.target.nodeName === 'BUTTON' ? e.target : e.target.parentNode;
+    target.classList.add('active');
+
     projectContainer.classList.add('animate')
 
     setTimeout(() => {
