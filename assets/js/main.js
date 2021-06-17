8f70ab0da91c5848d271631b0351aac502a0dea6
whdqja328 <whdqja328@naver.com>
2021-06-17 18:53:11 +0900

    JS_Observer_API

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,23 +3,19 @@
 // 스크롤시 nav메뉴 변경
 const navbar = document.querySelector('#nav');
 const navbarHeight = navbar.getBoundingClientRect().height;
-
 document.addEventListener('scroll', () => {
-
     if (window.scrollY > navbarHeight) {
         navbar.classList.add('nav_dark')
     } else {
         navbar.classList.remove('nav_dark')
     }
-
 });
 
 // nav 버튼 클릭시 해당 섹션 이동
-const navMenu = document.querySelector('ul.nav_menu');
+const navMenu = document.querySelector('.nav_menu');
 navMenu.addEventListener('click', (event) => {
     const target = event.target;
     const link = target.dataset.link;
-
     if (link == null) {
         return;
     }
@@ -29,14 +25,14 @@ navMenu.addEventListener('click', (event) => {
 
 // nav toggle 버튼
 const navToggleBtn = document.querySelector('.nav_toggle_btn');
-navToggleBtn.addEventListener('click',()=>{
+navToggleBtn.addEventListener('click', () => {
     navMenu.classList.toggle('on')
 });
 
 // contact 섹션 이동
 const homeContactBtn = document.querySelector('.home_contact');
 homeContactBtn.addEventListener('click', () => {
-    scrollIntoView('contact');
+    scrollIntoView('#contact');
 });
 
 // 스크롤시 home 섹션 투명도 조정
@@ -57,7 +53,7 @@ document.addEventListener('scroll', () => {
 });
 
 scrollTopBtn.addEventListener('click', () => {
-    scrollIntoView('home');
+    scrollIntoView('#home');
 })
 
 // Project 탭 메뉴
@@ -73,14 +69,14 @@ workBtnContainer.addEventListener('click', (e) => {
 
     // 해당 페이지 클래스 추가
     const active = document.querySelector('.category_btn.active');
-    active.classList.remove('active')
-    const target = e.target.nodeName === 'BUTTON' ? e.target : e.target.parentNode;
-    target.classList.add('active');
+    if(active != null){
+        active.classList.remove('active')
+    }
+    e.target.classList.add('active')
 
     projectContainer.classList.add('animate')
-
     setTimeout(() => {
-        projects.forEach((project) => {
+        projects.forEach(project => {
             if (filter === '*' || filter === project.dataset.type) {
                 project.classList.remove('on')
             } else {
@@ -91,16 +87,72 @@ workBtnContainer.addEventListener('click', (e) => {
     }, 300);
 });
 
-
-
-
-
-
-
-//  부드러운 스크롤 함수
-const scrollIntoView = (sel) => {
-    const scrollTo = document.getElementById(sel);
+// 스크롤 smooth 함수
+function scrollIntoView (sel) {
+    const scrollTo = document.querySelector(sel);
     scrollTo.scrollIntoView({
         behavior: "smooth"
     });
 }
+
+// IntersectionObserver API
+const sectionIds = [
+    '#home',
+    '#about',
+    '#skills',
+    '#work',
+    '#contact',
+  ];
+  
+  const sections = sectionIds.map(id => document.querySelector(id));
+  const navItems = sectionIds.map(id => document.querySelector(`[data-link="${id}"]`));
+  
+  let selectedNavIndex = 0;
+  let selectedNavItem = navItems[0];
+  function selectNavItem(selected) {
+    selectedNavItem.classList.remove('white');
+    selectedNavItem = selected;
+    selectedNavItem.classList.add('white');
+  };
+  
+  function scrollIntoView(selector) {
+    const scrollTo = document.querySelector(selector);
+    scrollTo.scrollIntoView({ behavior: 'smooth' });
+    selectNavItem(navItems[sectionIds.indexOf(selector)]);
+  };
+  
+  const observerOptions = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.3,
+  };
+  
+  const observerCallback = (entries, observer) => {
+    entries.forEach(entry => {
+      if(!entry.isIntersecting && entry.intersectionRatio > 0) {
+        const index = sectionIds.indexOf(`#${entry.target.id}`);
+        // 스크롤링이 아래로 되어서 페이지가 올라옴
+        if(entry.boundingClientRect.y < 0) {
+          selectedNavIndex = index +1;
+        } else {
+          selectedNavIndex = index -1;
+        }
+      };
+    });
+  };
+  
+  const observer = new IntersectionObserver(observerCallback, observerOptions);
+  sections.forEach(section => observer.observe(section));
+  
+  window.addEventListener('wheel', () => {
+    if (window.scrollY === 0) {
+      selectedNavIndex = 0;
+    } else if (
+      Math.round(window.scrollY + window.innerHeight) >=
+      document.body.clientHeight
+    ) {
+      selectedNavIndex = navItems.length - 1;
+    }
+    selectNavItem(navItems[selectedNavIndex]);
+  });
+
